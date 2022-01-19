95d1d749ee6a9a0dff63f7c17bfbbc7fa482a786
Eslam Ashraf <Eslam.ashrafff@yahoo.com>
2022-01-19 21:58:10 +0200

    added the ability to select location on map

--- lib/helpers/location_helper.dart
+++ lib/helpers/location_helper.dart
@@ -11,9 +11,10 @@ class LocationHelper {
     return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=${APIkeys.GOOGLE_API_KEY}';
   }
 
-  static Future<String> getPlaceAddress(double lat, double lng) async {
+  static Future<String> getPlaceAddress(
+      double latitude, double longitude) async {
     final url =
-        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=${APIkeys.GOOGLE_API_KEY}';
+        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$latitude,$longitude&key=${APIkeys.GOOGLE_API_KEY}';
     final response = await http.get(Uri.parse(url));
     return json.decode(response.body)['results'][0]['formatted_address'];
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 
 import './providers/great_places.dart';
@@ -6,7 +8,12 @@ import './screens/places_list_screen.dart';
 import './screens/add_place_screen.dart';
 import './screens/place_detail_screen.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  if (defaultTargetPlatform == TargetPlatform.android) {
+    AndroidGoogleMapsFlutter.useAndroidViewSurface = true;
+  }
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
